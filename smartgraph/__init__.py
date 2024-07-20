fa957050234b34854d8ecda7232eca50758598aa
Mohamed <jeblister@gmail.com>
2024-07-20 12:03:57 +0100

    clean_up_code to move to a reactive approach

--- smartgraph/__init__.py
+++ smartgraph/__init__.py
@@ -1,18 +1,7 @@
 # smartgraph/__init__.py
 
-from smartgraph.components import AggregatorComponent
-
 from .assistant_conversation import ReactiveAssistantConversation
-from .components import (
-    AggregatorComponent,
-    AsyncAPIComponent,
-    BranchingComponent,
-    CacheComponent,
-    FilterComponent,
-    RetryComponent,
-    TransformerComponent,
-    ValidationComponent,
-)
+from .component import ReactiveAIComponent
 from .core import ReactiveEdge, ReactiveNode, ReactiveSmartGraph
 from .exceptions import (
     ConfigurationError,
@@ -23,8 +12,6 @@ from .exceptions import (
     ValidationError,
 )
 from .logging import SmartGraphLogger
-
-# You can also include any utility functions or constants if you've defined any
 
 __all__ = [
     "ReactiveAIComponent",
@@ -39,14 +26,6 @@ __all__ = [
     "ValidationError",
     "MemoryError",
     "GraphStructureError",
-    "AggregatorComponent",
-    "FilterComponent",
-    "TransformerComponent",
-    "BranchingComponent",
-    "AsyncAPIComponent",
-    "RetryComponent",
-    "CacheComponent",
-    "ValidationComponent",
 ]
 
-__version__ = "0.2.0"  # Update this to reflect the new reactive version
+__version__ = "0.2.0"
